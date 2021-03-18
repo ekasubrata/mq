e79e625f714fda9834e7d0214bc5cbb67c17510d
idasbrt <idaayuekaputri.subrata@students.mq.edu.au>
2021-03-18 11:30:53 +0000

    assignment 1, question 3, part a

--- curl.sh
+++ curl.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-#save as curl.sh
-#downloads the link below and save it as case-cp.sh
 curl https://www.staff.hs-mittweida.de/~wuenschi/data/media/compbiolbook/chapter-10-shell-programming--case-cp.sh > case-cp.sh

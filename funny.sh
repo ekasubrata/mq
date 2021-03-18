8067acbc15cdd9b67d9415343a049981ea545478
idasbrt <idaayuekaputri.subrata@students.mq.edu.au>
2021-03-18 11:24:36 +0000

    assignment 1, question 2

--- funny.sh
+++ funny.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-#save as funny.sh
-#prints out "This is funny" when the script is invoked with an argument
-#prints out "This is NOT funny" when the script is invoked without an argument
 if test $# -ge 1; then
 	echo "This is funny"
 else
